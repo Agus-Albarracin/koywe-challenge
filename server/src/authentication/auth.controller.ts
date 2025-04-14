3d6eb5fe9b14a54fb99454b6479284e93a1086b4
Agus-Albarracin <abusscin_@hotmail.com>
2025-04-14 00:47:36 -0300

    feat(logout): Se agrega función logout para eliminar token dentro de las cookies

--- server/src/authentication/auth.controller.ts
+++ server/src/authentication/auth.controller.ts
@@ -1,5 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+  /**
+   * AuthController
+   * 
+   *
+   * Nota: 
+   * 
+   * Logout:
+   * - Elimina la cookie `access_token` que contiene el JWT del usuario.
+   * - La cookie se configura como `httpOnly` para evitar acceso desde JavaScript en el cliente.
+   * - `sameSite: 'lax'` ayuda a prevenir ataques CSRF sin bloquear solicitudes legítimas de navegación.
+   * - `secure: false` indica que la cookie puede ser enviada por HTTP, 
+   *   pero debe configurarse como `true` en producción con HTTPS (sugerido manejar con variable de entorno).
+   * - No requiere body, ya que solo limpia la cookie de sesión.
+   * 
+   */
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -13,5 +29,15 @@ export class AuthController {
   @Post('signin')
   signin(@Body() dto: { username: string; password: string }) {
     return this.authService.signin(dto.username, dto.password);
+  }
+
+  @Post('logout')
+  logout(@Res({ passthrough: true }) res: Response) {
+    res.clearCookie('access_token', {
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: false, // Podemos manejarlo con .env
+    });
+    return { message: 'Sesión cerrada correctamente' };
   }
 }
